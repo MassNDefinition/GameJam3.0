d8baa9749585d6c6319f7c6ba293064933399155
DCalic <dimitrijecalic@yahoo.com>
2018-12-08 16:52:55 +0100

    Added directional lightning which will follow the mouse cursor

---
+++ GameJam3.0/Assets/Scripts/Components/DirectionalLightComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightComponent : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 directionVector = (mousePosition - player.transform.position).normalized;
+        
+        float angleOffset = GameManager.GM.bulletPositionOffset;
+        mousePosition.x += Random.Range(-angleOffset, angleOffset);
+        mousePosition.y += Random.Range(-angleOffset, angleOffset);
+        mousePosition.z = 0;
+
+        float angle = Mathf.Atan2(-directionVector.y, -directionVector.x) * Mathf.Rad2Deg + 90;
+        
+        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+}
