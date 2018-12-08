76c5c4a2a97cab099c20dcc8bca874eb328c9c1a
DCalic <dimitrijecalic@yahoo.com>
2018-12-08 16:03:59 +0100

    Modifier Main Camera
    Camera will now follow the player with the small offset towards the mouse cursor.

---
+++ GameJam3.0/Assets/Scripts/Components/CameraComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraComponent : MonoBehaviour {
+    public GameObject player;
+    public float offsetModifier;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 directionVector = (mousePosition - player.transform.position).normalized;
+
+        transform.position = player.transform.position + offset + directionVector * offsetModifier;
+	}
+}
