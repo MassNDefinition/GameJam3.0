dae36546dd33b505802c51cfaf65bcb12e1ab2c9
DESKTOP-TANB250\Reggnos <stankovicmateja@yahoo.com>
2018-12-08 14:58:39 +0100

    Added sounds lights and textures

---
+++ GameJam3.0/Assets/LightSwitchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchController : MonoBehaviour {
+
+    public MeshRenderer[] lights;
+
+	// Use this for initialization
+	void Start () {
+        lights = GetComponentsInChildren<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E)){
+            foreach (MeshRenderer renderer in lights)
+                if (renderer.enabled == false)
+                {
+                    renderer.enabled = true;
+                }
+                else
+                {
+                    renderer.enabled = false;
+                }
+                
+
+        }
+	}
+}
