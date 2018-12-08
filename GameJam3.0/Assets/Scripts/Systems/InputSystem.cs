90b9ddaaa02d9f978210e27bde6259b59462818f
MassNDefinition <highlnadersrb@gmail.com>
2018-12-08 10:34:38 +0100

    Setting up ECS
    Setting up the ECS system.

---
+++ GameJam3.0/Assets/Scripts/Systems/InputSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class InputSystem : ComponentSystem
+{
+
+	struct Components
+    {
+        public InputEffectedComponent inputComponent;
+    }
+	
+	// Update is called once per frame
+	protected override void OnUpdate ()
+    {
+		foreach (var entity in GetEntities<Components>())
+        {
+
+        }
+	}
+}
