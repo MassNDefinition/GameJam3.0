22260a49e8489f7bfb332e0078be579612c8c8b5
DCalic <dimitrijecalic@yahoo.com>
2018-12-09 02:10:56 +0100

    Scenes

---
+++ GameJam3.0/Assets/Scripts/Managers/Cutscene1Manager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene1Manager : MonoBehaviour {
+
+    private int mouseClicks;
+
+	// Use this for initialization
+	void Start () {
+        mouseClicks = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0))
+        {
+            switch(mouseClicks)
+            {
+                case 0:
+                    ++mouseClicks;
+                    //show first dialog
+                    break;
+                case 1:
+                    ++mouseClicks;
+                    //show second dialog
+                    break;
+                case 2:
+                    ++mouseClicks;
+                    //show third dialog
+                    break;
+                case 3:
+                    ++mouseClicks;
+                    SceneManager.LoadScene("First Swarm");
+                    break;
+            }
+        }
+	}
+}
